25384881d4caf9ff488222dd74c9edb88f032935
FulconisYan <yan.fulconis@gmail.com>
2023-03-27 19:03:23 +0200

    Add models on API part

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ListIngredientsComponent } from './list-ingredients/list-ingredients.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListIngredientsComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
